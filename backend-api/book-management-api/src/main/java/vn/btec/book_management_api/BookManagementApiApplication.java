15fab8a7ca86778843552bfded6e70a1e076e985
trongcode <167011645+trongcode@users.noreply.github.com>
2025-02-14 08:13:27 +0700

    update code

---
+++ backend-api/book-management-api/src/main/java/vn/btec/book_management_api/BookManagementApiApplication.java
@@ -0,0 +1,13 @@
+package vn.btec.book_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementApiApplication.class, args);
+	}
+
+}
---
+++ backend-api/book-management-api/src/test/java/vn/btec/book_management_api/BookManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.btec.book_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
